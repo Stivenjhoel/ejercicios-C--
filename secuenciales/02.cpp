b68db052aa739c470a9a5b2521fe6dd0909dfeea
Stivenjhoel <stivenalbornoz31a@gmail.com>
2024-12-03 18:23:13 -0500

    Update 02.cpp

--- secuenciales/02.cpp
+++ secuenciales/02.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double metros;
+    cout << "Ingrese metros: ";
+    cin >> metros;
+
+    double centimetros = metros * 100;
+    double pulgadas = centimetros / 2.54;
+    double pies = pulgadas / 12;
+    double yardas = pies / 3;
+
+    cout << fixed << setprecision(2);
+    cout << "Centímetros: " << centimetros << endl;
+    cout << "Pulgadas: " << pulgadas << endl;
+    cout << "Pies: " << pies << endl;
+    cout << "Yardas: " << yardas << endl;
+
+    return 0;
+}
 
